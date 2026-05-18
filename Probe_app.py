f869bfb0c90c7d8fe93fa33e3fe18e6de4a315d9
Osher Rafaeli <osherr@post.bgu.ac.il>
2026-05-18 09:56:24 +0300

    Update Probe_app.py

--- Probe_app.py
+++ Probe_app.py
@@ -144,7 +144,7 @@ def process_file(uploaded_file):
     # remove moving GPS points inside each profile
     parts = []
     
-    point_radius_m = 1000
+    point_radius_m = 30
 
     for loc, g in df.groupby("location_id"):
         center_lat = g[LAT_COL].median()
